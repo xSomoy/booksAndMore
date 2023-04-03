f7de8de49a78563f918448566b25d01057d16fc6
John Deadman <xSomoy@protonmail.com>
2023-04-03 11:05:56 +0600

    ajax is working

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("underDev").innerHTML =
+        this.responseText;
+      }
+    };
+    xhttp.open("GET", "./snips/underDev.jsx", true);
+    xhttp.send();
+};
+
+loadDoc();
+
+// AJAX is working
