0be0b19e9908afb50bdfc1732a6a234e88537807
John Deadman <xSomoy@protonmail.com>
2023-04-06 00:43:01 +0600

    creating variables for common values

--- scripts/jsonParser.js
+++ scripts/jsonParser.js
@@ -1,4 +1,15 @@
+// Book Size 
+
 let size0 = "Regular / Hard Cover"
 let size1 = "Crown / Hard Cover"
 let size2 = "Crown / Paperback"
 let size3 = "Regular / Paperback"
+
+// Book Genre 
+
+let genre0 = "গল্পগ্রন্থ"
+let genre1 = "সমকালীন"
+let genre2 = "রোমান্টিক"
+let genre3 = "ক্রাইম থ্রিলার"
+let genre4 = "থ্রিলার"
+let genre5 = "কবিতা"
