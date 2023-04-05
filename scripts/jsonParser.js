7cf1a7b80b5266fe6e195e87e8d731d878fb3837
John Deadman <xSomoy@protonmail.com>
2023-04-06 01:14:52 +0600

    json is fucking me

--- scripts/jsonParser.js
+++ scripts/jsonParser.js
@@ -17,11 +17,30 @@ let genre6 = "মোটিভেশনাল";
 let genre7 = "ঐতিহাসিক থ্রিলাস";
 let genre8 = "মৌলিক উপকথা";
 
-function countJSONObjects(json) {
-  // Parse the JSON string into an object
-  const obj = JSON.parse(json);
-  // Get an array of keys in the top-level object
-  const keys = Object.keys(obj);
-  // Count the number of keys
-  return keys.length;
-}
+// function countJSONObjects(json) {
+//   // Parse the JSON string into an object
+//   const obj = JSON.parse(json);
+//   // Get an array of keys in the top-level object
+//   const keys = Object.keys(obj);
+//   // Count the number of keys
+//   return keys.length;
+// }
+
+// const json = "../bookList/booka.json";
+// console.log(countJSONObjects(json)); // Output: 2
+// const xhr = new XMLHttpRequest();
+// xhr.onreadystatechange = function() {
+//   if (xhr.readyState === 4 && xhr.status === 200) {
+//     const json = xhr.responseText;
+//     const obj = JSON.parse(json);
+//     console.log(obj);
+//   }
+// };
+// xhr.open('GET', '../bookList/books.json');
+// xhr.send();
+
+  fetch('books.json')
+  .then(response => response.json())
+  .then(data => console.log(data));
+
+
