29f7e09c5eafd6efba394ec4661096fc69c947bd
John Deadman <xSomoy@protonmail.com>
2023-04-06 06:19:28 +0600

    json test setup

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,45 @@
-"use strict";
-function loadDoc() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("underDev").innerHTML =
-        this.responseText;
-      }
-    };
-    xhttp.open("GET", "./snips/underDev.ajax", true);
-    xhttp.send();
-};
+// Book Size 
 
-loadDoc();
+let size0 = "Regular / Hard Cover";
+let size1 = "Crown / Hard Cover";
+let size2 = "Crown / Paperback";
+let size3 = "Regular / Paperback";
 
-// AJAX is working
+// Book Genre 
 
+let genre0 = "গল্পগ্রন্থ";
+let genre1 = "সমকালীন";
+let genre2 = "রোমান্টিক";
+let genre3 = "ক্রাইম থ্রিলার";
+let genre4 = "থ্রিলার";
+let genre5 = "কবিতা";
+let genre6 = "মোটিভেশনাল";
+let genre7 = "ঐতিহাসিক থ্রিলাস";
+let genre8 = "মৌলিক উপকথা";
+
+
+// Fucntion JSON Loan 
+
+// function loadJson(){
+//     let xhttp = new XMLHttpRequest();
+//     xhttp.onreadystatechange = function(){
+//         if(xhttp.readyState == 4 && xhttp.status == 200){
+//             let data = JSON.parse(xhttp.responseText);
+//             document.getElementById("bList").innerHTML = data.name;
+//         }
+//         else {
+//             console.log("Error")
+//         }
+//     }
+//     xhttp.open("GET" , "../bookList/book00.json", true);
+//     xhttp.send()
+// }
+
+// loadJson()
+
+// fetch Method 
+fetch('../bookList/book00.json')
+    .then((response) => response.json())
+    .then((book) => document.getElementById("book-00_name").innerHTML = book.name)
+    .then((book) => document.getElementById("book-00-img").setAttribute("src" = book.url));
+    
