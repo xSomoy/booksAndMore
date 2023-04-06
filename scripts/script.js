59bbc3aef7ee43083d9c1f07afb840d1638751d3
John Deadman <xSomoy@protonmail.com>
2023-04-06 11:35:36 +0600

    innerHTML test

--- scripts/script.js
+++ scripts/script.js
@@ -15,23 +15,23 @@ let genre4 = "থ্রিলার";
 let genre5 = "কবিতা";
 let genre6 = "মোটিভেশনাল";
 let genre7 = "ঐতিহাসিক থ্রিলাস";
-let genre8 = "মৌলিক উপকথা"; +
+let genre8 = "মৌলিক উপকথা"; 
 
-let bookAdd = " <div class="accordion-item">\
-        <  h2 class="accordion-header" id = "books-00" >\
-            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"\
-                data-bs-target="#books00" aria-expanded="true" aria-controls="books-00">\
-                <div id="book-00_name"></div>\
-            </button>\
-        </h2 >\
-        <div id="books00" class="accordion-collapse collapse collapsed" aria-labelledby="books-00"\
-        data-bs-parent="#accordionExample">\
-        <div class="accordion-body">\
-            <img src="" alt="" id="book-00-img">\
-        </div>\
-    </div>\
-</div >\"
 
+
+let accordian = '<div class="accordion-item"> <h2 class="accordion-header" id="books-00"> <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#books00" aria-expanded="true" aria-controls="books-00"> <div id="book-00_name"></div></button></h2> <div id="books00" class="accordion-collapse collapse collapsed" aria-labelledby="books-00"data-bs-parent="#accordionExample"> <div class="accordion-body"> <img src="" alt="" id="book-00-img"> </div></div></div>'
+
+
+
+
+// fetch Method 
+fetch('../bookList/book00.json')
+    .then((response) => response.json())
+    .then((book) => document.getElementById("book-00_name").innerHTML = book.name)
+    .then((book) => imgDiv = document.getElementById("book-00-img"))
+    .then(imgDiv.setAttribute("src", book.url));
+
+// console.log((book.name))
 
 // Fucntion JSON Loan 
 
@@ -51,12 +51,3 @@ let bookAdd = " <div class="accordion-item">\
 // }
 
 // loadJson()
-
-// fetch Method 
-fetch('../bookList/book00.json')
-    .then((response) => response.json())
-    .then((book) => document.getElementById("book-00_name").innerHTML = book.name)
-    .then((book) => imgDiv = document.getElementById("book-00-img"))
-    .then(imgDiv.setAttribute("src", book.url));
-
-// console.log((book.name))
